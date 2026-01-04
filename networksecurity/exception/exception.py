531d5ef95006c308acd4f64535f88c65d3f1e33f
Tushar9184 <sabharwaltushar02@gmail.com>
2026-01-04 23:50:27 +0530

    test_data

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -10,7 +10,7 @@ def error_message_detail(error, error_detail: sys):
     ) 
     return error_message
 
-class CustomException(Exception):
+class NetworkSecurityException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message) 
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         a = 1 / 0
     except Exception as e:
         logger.logging.info("Divide by zero error")
-        raise CustomException(e, sys)
+        raise NetworkSecurityException(e, sys)
